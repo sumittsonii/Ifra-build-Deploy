9d5fb17dd2f7c15e58b49587db74e50153c60382
sumittsonii <73901584+sumittsonii@users.noreply.github.com>
2025-08-27 21:18:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GitHub Actions & GCP Artifact Registry!"
+    return """
+    <!doctype html>
+    <html><body style="font-family:system-ui, -apple-system, Segoe UI, Roboto, sans-serif; line-height:1.5; padding:2rem;">
+      <p>Hey there,</p>
+      <p>welcome to website deployed using a CI/CD pipeline.</p>
+      <p>Thanks,<br>Sumit</p>
+    </body></html>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
